f67d2647a7608a7764ef1d65362ea6caee22282c
ding <69354087+prgding@users.noreply.github.com>
2023-10-10 08:40:21 +0800

    Added content

---
+++ src/.vuepress/.temp/pages/Untitled.html.js
@@ -0,0 +1,14 @@
+export const data = JSON.parse("{\"key\":\"v-0c32809b\",\"path\":\"/Untitled.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"description\":\"反问： 1. 我的日常工作 2. 有入职培训吗？是什么样的？ 3. 实习期结束时，如何衡量绩效？ 4. 电脑是自带还是公司提供？ 5. 公司提供的话能否选择自带？\"},\"headers\":[],\"readingTime\":{\"minutes\":0.2,\"words\":59},\"filePathRelative\":\"Untitled.md\",\"autoDesc\":true}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
